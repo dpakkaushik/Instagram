407dc33afd6b67612d267ddd3e393fb3ae294e4f
dpakkaushik <dpakkaushik@github.com>
2026-05-17 18:16:54 +0530

    Pass secrets as env vars directly, remove .env step

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=False)  # env vars from GitHub Actions take priority
 
 GEMINI_API_KEY      = os.getenv("GEMINI_API_KEY", "")
 GROQ_API_KEY        = os.getenv("GROQ_API_KEY", "")
